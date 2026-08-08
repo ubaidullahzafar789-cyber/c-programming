a79cde4012abb232ee6252293c0282b88940a232
UBAID ULLAH <ubaidullahzafar789@gmail.com>
2026-08-08 22:19:06 +0500

    Create 04_logical_operators.c

---
+++ 02-Operators/04_logical_operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("\n----- Logical Operators -----\n");
+
+    printf("(a > 0 && b > 0) : %d\n", (a > 0 && b > 0));
+    printf("(a > 0 || b > 0) : %d\n", (a > 0 || b > 0));
+    printf("!(a > 0)         : %d\n", !(a > 0));
+
+    return 0;
+}
