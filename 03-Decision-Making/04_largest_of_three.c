8d7425f4d6dede1790915947bde7b5d1cfac5f2e
UBAID ULLAH <ubaidullahzafar789@gmail.com>
2026-08-11 21:03:06 +0500

    Create 04_largest_of_three.c

---
+++ 03-Decision-Making/04_largest_of_three.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("Enter third number: ");
+    scanf("%d", &c);
+
+    if (a >= b && a >= c)
+    {
+        printf("%d is the largest.\n", a);
+    }
+    else if (b >= a && b >= c)
+    {
+        printf("%d is the largest.\n", b);
+    }
+    else
+    {
+        printf("%d is the largest.\n", c);
+    }
+
+    return 0;
+}
