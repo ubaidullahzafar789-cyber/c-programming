2ebfab501d7c5e3aa9f665af04fcb9b415310a98
UBAID ULLAH <ubaidullahzafar789@gmail.com>
2026-08-12 19:41:45 +0500

    Create 06_grade_calculator.c

---
+++ 03-Decision-Making/06_grade_calculator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks;
+
+    printf("Enter your marks (0-100): ");
+    scanf("%d", &marks);
+
+    if (marks < 0 || marks > 100)
+    {
+        printf("Invalid marks.\n");
+    }
+    else if (marks >= 90)
+    {
+        printf("Grade: A\n");
+    }
+    else if (marks >= 80)
+    {
+        printf("Grade: B\n");
+    }
+    else if (marks >= 70)
+    {
+        printf("Grade: C\n");
+    }
+    else if (marks >= 60)
+    {
+        printf("Grade: D\n");
+    }
+    else
+    {
+        printf("Grade: F\n");
+    }
+
+    return 0;
+}
