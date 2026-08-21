6275cc8a6f2d3d67311a0e6e2f885f819ca9da72
UBAID ULLAH <ubaidullahzafar789@gmail.com>
2026-08-21 17:05:33 +0500

    Create 08_multiplication_table.c

---
+++ 04-Loops/08_multiplication_table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("\nMultiplication Table of %d\n", number);
+
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", number, i, number * i);
+    }
+
+    return 0;
+}
