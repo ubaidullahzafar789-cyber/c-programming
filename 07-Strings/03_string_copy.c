8ac5a171411065386bc16ef5897ea56af4016842
UBAID ULLAH <ubaidullahzafar789@gmail.com>
2026-09-03 16:21:12 +0500

    Create 03_string_copy.c

---
+++ 07-Strings/03_string_copy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char source[100];
+    char destination[100];
+
+    printf("Enter a string: ");
+    fgets(source, sizeof(source), stdin);
+
+    source[strcspn(source, "\n")] = '\0';
+
+    strcpy(destination, source);
+
+    printf("Original string: %s\n", source);
+    printf("Copied string: %s\n", destination);
+
+    return 0;
+}
